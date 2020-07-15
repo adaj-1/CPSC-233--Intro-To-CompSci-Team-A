4431060ac151a71ad2189bf41cebf3bfef58fe1f
njungg <68088497+njungg@users.noreply.github.com>
2020-07-14 18:56:53 -0600

    Updated Poll Class Team Mission 1
    Updated Poll Class Team Mission 1

--- Poll.java
+++ Poll.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Poll {
 	private String name = "Unnamed Poll";
 	private Party[] parties;
@@ -5,11 +8,16 @@ public class Poll {
 
 	public Poll(String name, int maxNumberOfParties) {
 		this.name = name; 
-		parties = new Party[maxNumberOfParties];
+		if(maxNumberOfParties<1) {
+			maxNumberOfParties=10;
+			parties = new Party[maxNumberOfParties];
+		}
+		else parties= new Party[maxNumberOfParties];	
 	}
 	
 	public String getPollName() {
-		return name;
+		return this.name;
+		
 	}
 	
 	public int getNumberOfParties() {
@@ -17,11 +25,41 @@ public class Poll {
 	}
 	
 	public void addParty(Party aParty) {
-		parties[numberOfParties++] = aParty;
+		if (aParty==null) {
+			System.out.println("Error");
+		}
+		Boolean exists=false;
+		Boolean size=true;
+		for(int i=0; i<parties.length&&parties[i]!=null; i++) {//Iterates through every index of parties array
+			if(aParty.getName().equalsIgnoreCase(parties[i].getName())) {
+				parties[i]=aParty; //Replaces existing party with same name with argument aParty
+				exists=true;
+			}
+			else if(numberOfParties>=parties.length) {
+				System.out.println("Error, poll is full. No more parties can be added");
+				size=false;
+			}
+			
+			
+			}	
+		
+		if(exists.equals(false)&&size.equals(true)) {
+			parties[numberOfParties++]=aParty;	
+		}
+	
 	}
-
+	
+	
 	public Party getParty(String name) {
-		return null;
+		
+		for(int i=0;i<parties.length;i++) {
+			if(name.toUpperCase().equals(parties[i].getName().toUpperCase())) {
+				return parties[i];
+			}
+			
+		}
+		 return null;
+		
 	}
 	
 	public Party[] getPartiesSortedBySeats() {
@@ -32,8 +70,21 @@ public class Poll {
 		return parties;
 	}
 	
+
+	
 	@Override
 	public String toString() {
-		return "";
+		StringBuilder sb= new StringBuilder();
+		for(int i=0; i<parties.length;i++) {
+			if(parties[i]==null) {
+				return name + "\n";
+			}
+			else if(parties.length>0) {
+				sb.append(parties[i].getName());
+				sb.append("\n");
+			}
+			
+		}
+		return String.format(name+"\n"+sb);
 	}
 }
