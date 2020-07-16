e00540487bb5c28cf1eca0c5a916408c45cc2274
adaj-1 <jada.li@ucalgary.ca>
2020-07-15 18:08:20 -0600

    TextApplication-07-15

--- TextApplication.java
+++ TextApplication.java
@@ -1,54 +1,104 @@
+/*
+ * TextApplication	Jada Li
+ * 
+ * Iteration 1
+ * 
+ */
+
 import java.util.Scanner;
+
+/**
+ * This class represents the entire application and pulls together 
+ * all other classes (Party, Poll, PollList, and Factory). It takes
+ * input from the user and outputs a visualization of the results of 
+ * an election.
+ * 
+ * @version 1  14 Jul 2020
+ * @author Jada Li
+ *
+ */
+
 public class TextApplication {
-	public static final int MAX_NUMBER_OF_STARS = 25;
-	private PollList polls;
+	/* FILL WITH CLASS DESCRIPTION OR DELETE  */
 	
+	/** MAX_NUMBER_OF_STARS represents seats in the visualization */
+	public static final int MAX_NUMBER_OF_STARS = 25;
+	
+	/** FILL WITH INSTANCE VARIABLE DESCRIPTION OR DELETE */
+		private PollList polls;
+	/**
+	 * default constructor for the class
+	 */
 	public TextApplication() {	
+	// DO I LEAVE A BLANK SPACE HERE?
 	}
 	
+	/**
+	 * initializes instance variable polls
+	 * @param polls this should provide the polls data
+	 */
 	public TextApplication(PollList polls) {
 		this.polls = polls;
 	}
 	
+	/**
+	 * takes the polls data and provides it to displayPollDataBySeat()
+	 * @param partyNames has all the party names
+	 */
 	public void displayPollsBySeat(String[] partyNames) {
+		
 		int numOfPolls = polls.getPolls().length;
-		Poll[] regularPoll = polls.getPolls();
+		Poll[] regularPoll = polls.getPolls();						// grabs array of poll data 
 		
 		for (int i = 0; i < numOfPolls; i++) {	
-			displayPollDataBySeat(regularPoll[i]);
-		  	
+			
+			displayPollDataBySeat(regularPoll[i]);					// invokes displayPollDataBySeat for each poll
+		  
 			System.out.println();
 		}
-		
-		Poll AggregatePoll = polls.getAggregatePoll(partyNames);
+			
+		Poll AggregatePoll = polls.getAggregatePoll(partyNames);	// invokes displayPollDataBySeat for the Aggregate poll data
 		displayPollDataBySeat(AggregatePoll);
 	}
 	
+	/**
+	 * gets the poll data from PollList
+	 * @return the ArrayList for the poll
+	 */	
 	public PollList getPolls() {
 		return polls;
 	}
-
+	
+	/**
+	 * invokes textVisualizationBySeats method to display visualization of 
+	 * the poll data for each poll and each party.
+	 * @param aPoll this is the name of the poll and number of parties
+	 */
 	public void displayPollDataBySeat(Poll aPoll) {
-		int starsNeededForMajority = (MAX_NUMBER_OF_STARS + 1) / 2; //majority;
 		
-		int numOfSeats = polls.getNumOfSeats();	
-		double numOfSeatsPerStar = numOfSeats / MAX_NUMBER_OF_STARS; 
+		int starsNeededForMajority = (MAX_NUMBER_OF_STARS + 1) / 2;		// calculates # of stars for majority
+		
+		int numOfSeats = polls.getNumOfSeats();  						// gets the # of seats for the poll
+		double numOfSeatsPerStar = numOfSeats / MAX_NUMBER_OF_STARS;	// determines # of seats per star
 				
 		int numOfParties = aPoll.getNumberOfParties();
-		Party[] partyList = aPoll.getPartiesSortedBySeats();
+		Party[] partyList = aPoll.getPartiesSortedBySeats();			// gets list of parties sorted by seats
 							
 		System.out.println(aPoll.getPollName());
 		
 		for (int i = 0; i < numOfParties; i++) {
-			String visualizationBySeat = partyList[i].textVisualizationBySeats(MAX_NUMBER_OF_STARS, 
+			String visualizationBySeat = partyList[i].textVisualizationBySeats(MAX_NUMBER_OF_STARS,			// creates string needed for visualization
 																			   starsNeededForMajority,
 																			   numOfSeatsPerStar);
 			System.out.println(visualizationBySeat);	
 		}
 	}
 	
-	
-	public void run() {
+	/**
+	 * this pulls all the classes (Party, Poll, PollList, and Factory)
+	 * together and gets the user input arguments needed to run the code.
+	 */
+	public void run()  {	
 		System.out.println("Welcome To The Poll Tracker.");
 		
 		System.out.println("How many seats are available in the election?");
@@ -70,43 +120,45 @@ public class TextApplication {
 		String yesNo = yesNoInput.nextLine();
 		
 		if (yesNo.equals("yes")) {
-			Factory test = new Factory(numOfSeats);	//generates random polls using Factory
-			polls = test.promptForPollList(numOfPolls);
-			
+			Factory holdPollList = new Factory(numOfSeats);        		// determine whether to use random polls or user input polls
+			polls = holdPollList.createRandomPollList(numOfPolls);		// creates a random poll list data
 		} else {
-			polls = new PollList(numOfPolls, numOfSeats);	//generate polls using other classes
-			
+			Factory holdPollList = new Factory(numOfSeats);				
+			polls = holdPollList.promptForPollList(numOfPolls);			// prompts user for poll list data			
 		}
 		
 		boolean hold = true;
 		do {
-			System.out.println("Options: all (Show results of all Polls),"
+			System.out.println("Options: all (Show results of all Polls),"					/* 
+			FIX COMMENT HERE
+			 */
 					+ " aggregate (Show Aggregate results), or quit (End application)");
 			System.out.println("Choose an Option: ");
 			Scanner optionInput = new Scanner(System.in);
 			String option = optionInput.nextLine();	
 			
-			if (option.equals("all")) {
-				System.out.println("IN ALL");
+			if (option.equals("all")) {														// displays all poll data including aggregate
 				displayPollsBySeat(partyNameList);
 				hold = true;
-				
-			} else if (option.equals("aggregate")) {
-				System.out.println("IN AGGREGATE");
+			} else if (option.equals("aggregate")) {										// displays only the aggregate data
 				Poll AggregatePoll = polls.getAggregatePoll(partyNameList);
 				displayPollDataBySeat(AggregatePoll);
 				hold = true;
 			} else {
-				System.out.println("Goodbye.");
+				System.out.println("Goodbye.");												// ends the application
+				optionInput.close();
 				hold = false;
 			}	
-		}while(hold == true);	
-	
+		} while (hold == true);	
+		
+		seatinput.close();					// closes resource 
+		partyNameInput.close();
+		numOfPollsInput.close();
+		yesNoInput.close();	
 	}
 
 	public static void main(String[] args) {
-		
-		TextApplication app = new TextApplication();
+		TextApplication app = new TextApplication();		// instantiates the class
 		app.run();	
 	}
 }
