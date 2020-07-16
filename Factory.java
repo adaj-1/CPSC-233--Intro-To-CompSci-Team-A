9b02ae725d5c2f93e5b2e3dc4f6e0037ae7d24a4
randmann <67571972+randmann@users.noreply.github.com>
2020-07-15 19:09:55 -0600

    Add files via upload

--- Factory.java
+++ Factory.java
@@ -7,10 +7,10 @@ public class Factory {
 
 	public Factory(int seats) {
 		if (numOfSeats >= 1) {
-		seats = numOfSeats;
+			seats = numOfSeats;
+		}
 	
 	}
-}
 	
 	public String[] getPartyNames() {
 			return partyNames;
@@ -23,44 +23,52 @@ public class Factory {
 		Random rand= new Random();
 			int projSeats = rand.nextInt(maxSeats);
 			double percentSeats = projSeats/numOfSeats;
-			int randomVote = rand.nextInt(maxPercentOfVotes/100);
+			if (maxPercentOfVotes >= 0 && maxPercentOfVotes <= 100) {
+				int randomVote = rand.nextInt(maxPercentOfVotes/100);
 
-		if (Math.abs(percentSeats - randomVote) < 5) {
-		Party party = new Party(partyName, maxSeats, maxPercentOfVotes);
-		return party;
+			if (Math.abs(percentSeats - randomVote) < 5) {
+				Party party = new Party(partyName, maxSeats, maxPercentOfVotes);
+				return party;
 						
 			}
-		return null; 
+			}
+				return null;
 			}
 
 	
 	public Poll createRandomPoll(String name) {
 		Random rand = new Random();
 		double percentVotes = rand.nextFloat();
+		
 		if (percentVotes >= 0 && percentVotes <= 100) {
-		for (String partyname : partyNames) {
-		createRandomParty(partyname, rand.nextInt(numOfSeats), rand.nextInt((int)percentVotes));
+			for (String partyname : partyNames) {
+				createRandomParty(partyname, rand.nextInt(numOfSeats), rand.nextInt((int)percentVotes));
 	} 
 		} 
-		for (int i = 0; i < partyNames.length; i++) { 
-		int projNumofSeats = rand.nextInt();
-		int totalprojSeats = projNumofSeats;
-		if (totalprojSeats < numOfSeats) {
-		Poll poll = new Poll(name, partyNames.length);
-		return poll;
+		
+		for (int i = 0; i < partyNames.length; i++) {
+			int projNumofSeats = rand.nextInt(numOfSeats);
+			int totalprojSeats = projNumofSeats;
+			
+			if (totalprojSeats < numOfSeats) {
+				Poll poll = new Poll(name, partyNames.length);
+				return poll;
 	}
 		
 		}
-		return null;
+				return null;
 	}
 
 	public PollList createRandomPollList(int numOfPolls) {
 		Random rand = new Random();
-		PollList list = new PollList(numOfPolls,rand.nextInt(numOfSeats));
-		for (int counter = 0; counter < numOfPolls; counter++) {
-			list.addPoll(createRandomPoll("Poll" + counter));
+		if (rand.nextInt(numOfSeats) >= 0) {
+			PollList list = new PollList(numOfPolls,rand.nextInt(numOfSeats));
+			for (int counter = 0; counter < numOfPolls; counter++) {
+				list.addPoll(createRandomPoll("Poll" + counter));
 		}
-		return list;
+			return list;
+		}
+			return null;
 	}
 	
 	public PollList promptForPollList(int numOfPolls) {
