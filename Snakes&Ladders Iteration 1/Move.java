1d1e4e3015cd2db9982eb35da231207267302205
adaj-1 <jada.li@ucalgary.ca>
2020-07-21 18:09:20 -0600

    Updated Move

--- Snakes&Ladders Iteration 1/Move.java
+++ Snakes&Ladders Iteration 1/Move.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Move { //Jada
+public class Move { // Jada
 	
 	public Move(Player player) {	
 		int position = player.getPosition();
@@ -9,19 +9,12 @@ public class Move { //Jada
 		newPosition = Ladder(newPosition);
 		newPosition = Snake(newPosition);
 		
-		player.setPosition(newPosition);
-		// calls get method to getPosition
-		// dice method and adds to the player index
-		// check if snake or ladder by calling methods below and passing back the index
-		// call set method to setPosition	
+		if (newPosition <= 100) {
+			player.setPosition(newPosition);
+		} else if(newPosition > 100) {
+			player.setPosition(position);
+		}
 	}
-
-	/*
-	 *  move will take the dice roll and add that to the player index ( to move the player)
-	 *  then it will check if that number lands on a ladder or a snake. 
-	 *  then it will clear the players previous index with a blank " " so that player does not
-	 *  appear in two spots on the game board
-	 */
 
 	public int Ladder(int position) {
 		int[] laddersStart = new int[3];		
@@ -64,9 +57,9 @@ public class Move { //Jada
 		}
 		return position;	
 	}
-
+	
 	public int dice() {
-		int max = 7;
+		int max = 6;
 		int min = 1;
 		Random randomNum = new Random();
 		int diceRoll = randomNum.nextInt((max - min) + 1) + min; // Source: https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
