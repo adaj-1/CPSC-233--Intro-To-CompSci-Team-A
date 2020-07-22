cf054e521d828670e623a5abc648d38912c5221b
njungg <68088497+njungg@users.noreply.github.com>
2020-07-21 21:24:31 -0600

    Update Player.java

--- Snakes&Ladders Iteration 1/Player.java
+++ Snakes&Ladders Iteration 1/Player.java
@@ -5,34 +5,23 @@ public class Player { // Arlina
 
 	private String name; 
 	private int position = 0;
-	private ArrayList <Player> PlayerList = new ArrayList <Player> (); //arrayList of player objects
+
 
 	public Player() {
-		
+		String[] player = new String[101];
+		for (int i = 0; i < 101; i++) {
+			player[i] = "  ";
+		}
 	}
 	
+	
+	
 	public Player(String name) {
-		
 		this.name = name;
 		
 	}
 
-	public Player(String playerType, String playerName) {
-		/*
-		 * loop String[] to fill the arrayList for 4 players
-		 */
-
-		String[] player = new String[101];
-		for (int i = 0; i < 101; i++) {
-			player[i] = " ";
-		}
-
-		for (int i = 0; i < 4; i++) {
-			PlayerList.add(new Player(playerName));
-		}
-
-
-	}
+	
 	
 	public String getName() {
 		return name;
