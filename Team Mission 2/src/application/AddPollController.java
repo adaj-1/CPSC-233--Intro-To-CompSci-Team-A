96695e9a6e8b9461aa46de3507a9770ef3178da2
njungg <68088497+njungg@users.noreply.github.com>
2020-07-29 23:13:34 -0600

    Updated addPoll with comments
    Updated addPoll with comments

--- Team Mission 2/src/application/AddPollController.java
+++ Team Mission 2/src/application/AddPollController.java
@@ -20,46 +20,68 @@ import java.util.ArrayList;
 
 import javafx.collections.FXCollections;
 
-
-
-
 public class AddPollController extends PollTrackerController{
-	
+	/**
+	 * This label is for the drop down of list of current polls
+	 */
     @FXML
-    private Label pollPlacement;
-
+    private Label currentPolls;
+	/**
+	 * This Button is used for naming a poll
+	 */
     @FXML
     private Button addPoll;
 
+	/**
+	 * This label indicates to enter a name of a poll into a text field
+	 */
     @FXML
     private Label PollName;
+	/**
+	 * This button is used for clearing all inputs.
+	 */
 
     @FXML
     private Button clearUserFields;
 
+	/**
+	 * This choice box contains the list of Poll names that will be 
+	 * used in a poll list
+	 */
     @FXML
     private ChoiceBox<String> pollDropDown;
-
+    
+    /**
+     * This text field is where the user can input the Poll name into
+     * of their choice to replace the current place holder names
+     */
     @FXML
     private TextField userInput;
     	
-
+   /**
+ 	* This method handles the event when the clear button is clicked.
+ 	* It clears all input.
+ 	* @param event
+ 	*/
     @FXML
     void clear(ActionEvent event) {
     		userInput.clear();
-    		pollDropDown.getSelectionModel().clearSelection();
-    
+    		refresh();
     }
     
-   
-
+    /**
+     * This method handles the event when the addPoll button is clicked. 
+     * It will take the name entered in the text box and then replace the poll name that was selected from the drop down.
+     *  
+     * @param event
+     */
     @FXML
     void addPoll(ActionEvent event) {
-    	Poll RandomPoll=getFactory().createRandomPoll(userInput.getText());
+    	Poll randomPolls=getFactory().createRandomPoll(userInput.getText());
     	Poll[] poll=getPollList().getPolls();
     	
     	int indexNum=pollDropDown.getSelectionModel().getSelectedIndex();
-    	poll[indexNum]=RandomPoll;
+    	poll[indexNum]=randomPolls;
     	
     	PollList newList=new PollList(poll.length,getPollList().getNumOfSeats());
     	for(int i=0;i<poll.length;i++) {
@@ -68,21 +90,21 @@ public class AddPollController extends PollTrackerController{
     	setPollList(newList);
     }
 
-    @FXML
-    void userInputType(ActionEvent event) {
-    	
-    }
-
+    /**
+     * This method refreshes the list of polls with any changes made from the 
+     * text field and the drop down of the desired poll name to change.
+     * Outputs a message to indicate a refresh has been made.
+     */
 	@Override
 	public void refresh() {
+		ArrayList<String> pollList=new ArrayList<String>();
 		Poll[] poll=getPollList().getPolls();
-		ArrayList<String> list=new ArrayList<String>();
 		for(int i=0;i<poll.length;i++) {
-			list.add(poll[i].getPollName());
+			pollList.add(poll[i].getPollName());
 		}
-			pollDropDown.setItems(FXCollections.observableArrayList(list));
+			pollDropDown.setItems(FXCollections.observableArrayList(pollList));
 			System.out.println("Refreshing");
 		
-		
+		userInput.clear();
 	}
 }
