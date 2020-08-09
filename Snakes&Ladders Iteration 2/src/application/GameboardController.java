65764dacc706d8e9849c03de11c660482666588a
adaj-1 <jada.li@ucalgary.ca>
2020-08-08 22:03:10 -0600

    randomized snakes and ladders

--- Snakes&Ladders Iteration 2/src/application/GameboardController.java
+++ Snakes&Ladders Iteration 2/src/application/GameboardController.java
@@ -2,11 +2,13 @@ package application;
 
 
 import java.net.URL;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import model.GameConfiguration;
 import model.Player;
+import javafx.animation.PauseTransition;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -19,6 +21,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 
 
@@ -93,7 +96,7 @@ public class GameboardController {
     private GridPane gameGridTop;
     
     @FXML
-    private GridPane gameGridBottom;
+    private Pane pane;
   
     @FXML
     private Label currentplayerText;
@@ -102,11 +105,81 @@ public class GameboardController {
     private Label yourRoll;
     
     @FXML
-    private ImageView snake47;
+    private ImageView grass;
     
     @FXML
-    private ImageView grass;
+    private ImageView ladder4;
+
+    @FXML
+    private ImageView ladder55;
+    
+    @FXML
+    private ImageView ladder62;
+
+    @FXML
+    private ImageView ladder51;
+
+    @FXML
+    private ImageView ladder30;
+    
+    @FXML
+    private ImageView ladder18;
+    
+    @FXML
+    private ImageView ladder37;
+
+    @FXML
+    private ImageView ladder41;
+    
+    @FXML
+    private ImageView ladder87;
+
+    @FXML
+    private ImageView ladder14;
+
+    @FXML
+    private ImageView ladder58;
+    
+    @FXML
+    private ImageView ladder73;
+    
+    @FXML
+    private ImageView snake83;
+    
+    @FXML
+    private ImageView snake57;
+    
+    @FXML
+    private ImageView snake46;
+
+    @FXML
+    private ImageView snake26; 
+
+    @FXML
+    private ImageView snake93;
+    
+    @FXML
+    private ImageView snake52;
+    
+    @FXML
+    private ImageView snake96;
    
+    @FXML
+    private ImageView snake21;
+    
+    @FXML
+    private ImageView snake80;
+    
+    @FXML
+    private ImageView snake34;
+
+    @FXML
+    private ImageView snake16; 
+
+    @FXML
+    private ImageView snake97;
+    
+
     /**
      * takes user input for # of human players and # of computer players for GUI setup
      * @param event
@@ -124,14 +197,14 @@ public class GameboardController {
       		tokens.add(token2);
       		token1.setOpacity(1);
     		token2.setOpacity(1);
-      	}else if (totalPlayers == 3) {
+      	} else if (totalPlayers == 3) {
       		tokens.add(token1);
       		tokens.add(token2);
       		tokens.add(token3);
       		token1.setOpacity(1);
     		token2.setOpacity(1);
     		token3.setOpacity(1);
-      	}else if (totalPlayers == 4){
+      	} else if (totalPlayers == 4){
       		tokens.add(token1);
       		tokens.add(token2);
       		tokens.add(token3);
@@ -198,6 +271,69 @@ public class GameboardController {
     		} 
     	}
     	currentplayerText.setText(playerNameList[getTURN()]);
+    	
+    		if (gc.getLaddersStart()[0] == 4) {
+    			ladder4.setOpacity(1);
+    		} else {
+    			ladder37.setOpacity(1);
+    		}
+    		if (gc.getLaddersStart()[1] == 18) {
+				ladder18.setOpacity(1);
+			} else {
+				ladder41.setOpacity(1);
+			}
+    		if (gc.getLaddersStart()[2] == 30) {
+				ladder30.setOpacity(1);
+			} else {
+				ladder87.setOpacity(1);
+			}
+    		if (gc.getLaddersStart()[3] == 51) {
+				ladder51.setOpacity(1);
+			} else {
+				ladder14.setOpacity(1);
+			}
+    		if (gc.getLaddersStart()[4] == 55) {
+				ladder55.setOpacity(1);
+			} else {
+				ladder58.setOpacity(1);
+			}
+    		if (gc.getLaddersStart()[5] == 62) {
+				ladder62.setOpacity(1);				
+			} else {
+				ladder73.setOpacity(1);
+			}
+    		
+    		if (gc.getSnakesStart()[0] == 93) {
+    			snake93.setOpacity(1);
+    		} else {
+    			snake97.setOpacity(1);
+    		}
+    		if (gc.getSnakesStart()[1] == 83) {
+				snake83.setOpacity(1);
+			} else {
+				snake96.setOpacity(1);
+			}
+    		if (gc.getSnakesStart()[2] == 57) {
+				snake57.setOpacity(1);
+			} else {
+				snake21.setOpacity(1);
+			}
+    		if (gc.getSnakesStart()[3] == 52) {
+				snake52.setOpacity(1);
+			} else {
+				snake80.setOpacity(1);
+			}
+    		if (gc.getSnakesStart()[4] == 46) {
+				snake46.setOpacity(1);
+			} else {
+				snake34.setOpacity(1);
+			}
+    		if (gc.getSnakesStart()[5] == 26) {
+				snake26.setOpacity(1);				
+			} else {
+				snake16.setOpacity(1);
+			}
+    	
     }
     
     /**
@@ -214,6 +350,7 @@ public class GameboardController {
         		
         	} 	
     	} 
+    	
     }
     
     /*
@@ -235,6 +372,8 @@ public class GameboardController {
         			}
     			}
     	);
+    	
+    	gc.initializeSnakeLadder();
     }
     
     /**
@@ -265,6 +404,7 @@ public class GameboardController {
      * keeps track of player turns 
      */
     public void TurnCounter() { 
+  
     	if (getTURN() == (totalPlayers - 1)) {
 			this.TURN = 0;
 		} else {
@@ -299,7 +439,9 @@ public class GameboardController {
     */
     public void MovePlayerGUI(GameConfiguration gc) {
     	if (gc.getPlayer().get(getTURN()).getType() == "human") {
-    			Move();
+    		
+    		Move();
+    		
     	}    	
     } 
     
@@ -308,9 +450,14 @@ public class GameboardController {
      */
     public void moveComputerGUI() {  
     	if (gc.getPlayer().get(getTURN()).getType() == "AI")  {	
+    		rollDice.setDisable(true);
+    		waitfor();
     		Move();
+    		rollDice.setDisable(false);	
     		moveComputerGUI();
+    	
     	}
+    	
     } 
     
     /**
@@ -329,7 +476,8 @@ public class GameboardController {
     		yourRoll.setText(gc.getPlayer().get(getTURN()).getName() + " went down a snake and rolled a: " + gc.getPlayer().get(getTURN()).getHoldDiceRoll());
     		
     	} else {
-    		yourRoll.setText(gc.getPlayer().get(getTURN()).getName() + " you rolled a: " + gc.getPlayer().get(getTURN()).getHoldDiceRoll()); 		
+    		yourRoll.setText(gc.getPlayer().get(getTURN()).getName() + " you rolled a: " + gc.getPlayer().get(getTURN()).getHoldDiceRoll()); 
+    		
     	}
     	gameGridTop.getChildren().remove(tokens.get(getTURN()));
     	gameGridTop.add(tokens.get(getTURN()),columns[position],rows[position]);
@@ -342,17 +490,17 @@ public class GameboardController {
     /**
      * the below method is for iteration 3
      */
-//  public void waitfor() {
-//  	if (gc.getPlayer().get(getTURN()).getType() == "AI")  {
-//  		try
-//  		{
-//  		    Thread.sleep(3000);
-//  		}
-//  		catch(InterruptedException ex)
-//  		{
-//  		    Thread.currentThread().interrupt();
-//  		}	
-//  	 }	
-//  }
+  public void waitfor() {
+  	if (gc.getPlayer().get(getTURN()).getType() == "AI")  {
+  		try
+  		{
+  		    Thread.sleep(1000);
+  		}
+  		catch(InterruptedException ex)
+  		{
+  		    Thread.currentThread().interrupt();
+  		}	
+  	 }	
+  }
  
 }
--- Snakes&Ladders Iteration 2/src/model/Move.java
+++ Snakes&Ladders Iteration 2/src/model/Move.java
@@ -4,7 +4,11 @@ import java.util.Random;
 
 public class Move { // Jada
 	private int playerPosition;
+	private int[] snakesStartGUI = new int[6];
+	private int[] snakesEndGUI = new int[6];
 	
+	private int[] laddersStartGUI = new int[6];
+	private int[] laddersEndGUI = new int[6];
 	public Move() {
 		
 	}
@@ -58,18 +62,13 @@ public class Move { // Jada
 		
 		int validRoll=diceRoll+currentPosition;
 		if (validRoll <= 100) {
-			validRoll = Ladder(validRoll);
-			validRoll = Snake(validRoll);
+			validRoll = LadderGUI(validRoll);
+			validRoll = SnakeGUI(validRoll);
 		
 			currentPlayer.getPlayer()[validRoll]=currentPlayer.getName();
-			System.out.println("You rolled a " + diceRoll + ", and you landed on " + validRoll + ".");
 			currentPlayer.setPosition(validRoll);
 			return currentPlayer;
-		} else if (validRoll > 100) {
-			System.out.println("Rolled over 100, try again!");
-			
-		}
-		
+		}		
 		currentPlayer.getPlayer()[currentPosition]=currentPlayer.getName();
 		currentPlayer.setPosition(currentPosition);
 		
@@ -142,10 +141,198 @@ public class Move { // Jada
 		for (int counter = 0; counter < 4; counter++) {
 			if (position == snakesStart[counter]) {
 				position = snakesEnd[counter];
-				System.out.print("You went down a snake! ");
 			}
 		}
 		return position;	
+	}
+	
+	public boolean getRandomBoolean() {
+		Random random = new Random();
+		return random.nextBoolean();
+	}
+	
+	public int LadderGUI(int position) {
+		
+		
+		for (int counter = 0; counter < 6; counter++) {
+			if (position == laddersStartGUI[counter]) {
+				
+				position = laddersEndGUI[counter];
+			}
+		}
+		return position;	
+	}
+	
+	public int SnakeGUI(int position) {
+		
+		for (int counter = 0; counter < 6; counter++) {
+			if (position == snakesStartGUI[counter]) {
+				position = snakesEndGUI[counter];
+			}
+		}
+		return position;	
+	}
+	
+	public void initializeSnakeLadder() {
+		if (getRandomBoolean() == true) {
+			laddersStartGUI[0] = 4;
+		} else {
+			laddersStartGUI[0] = 37;
+		}
+		if (getRandomBoolean() == true) {
+			laddersStartGUI[1] = 18;
+		} else {
+			laddersStartGUI[1] = 41;
+		}
+		if (getRandomBoolean() == true) {
+			laddersStartGUI[2] = 30;
+		} else {
+			laddersStartGUI[2] = 87;
+		}
+		if (getRandomBoolean() == true) {
+			laddersStartGUI[3] = 51;
+		} else {
+			laddersStartGUI[3] = 14;
+		}
+		if (getRandomBoolean() == true) {
+			laddersStartGUI[4] = 55;
+		} else {
+			laddersStartGUI[4] = 58;
+		}
+		if (getRandomBoolean() == true) {
+			laddersStartGUI[5] = 62;
+		} else {
+			laddersStartGUI[5] = 73;
+		}
+		setLaddersStart(laddersStartGUI);
+		System.out.println("ladders start");
+		for (int i = 0; i < getSnakesStart().length; i++) {
+			System.out.println(getLaddersStart()[i]);
+		}
+		
+		
+		if (laddersStartGUI[0] == 4) {
+			laddersEndGUI[0] = 25;
+		} else {
+			laddersEndGUI[0] = 56;
+		}
+		if (laddersStartGUI[1] == 18) {
+			laddersEndGUI[1] = 39;
+		} else {
+			laddersEndGUI[1] = 59;
+		}
+		if (laddersStartGUI[2] == 30) {
+			laddersEndGUI[2] = 49;
+		} else {
+			laddersEndGUI[2] = 94;
+		}
+		if (laddersStartGUI[3] == 51) {
+			laddersEndGUI[3] = 72;
+		} else {
+			laddersEndGUI[3] = 33;
+		}
+		if (laddersStartGUI[4] == 55) {
+			laddersEndGUI[4] = 74;
+		} else {
+			laddersEndGUI[4] = 77;
+		}
+		if (laddersStartGUI[5] == 62) {
+			laddersEndGUI[5] = 96;
+		} else {
+			laddersEndGUI[5] = 92;
+		}
+		System.out.println("ladders end");
+		for (int i = 0; i < getSnakesStart().length; i++) {
+			System.out.println(laddersEndGUI[i]);
+		}
+		
+		if (getRandomBoolean() == true) {
+			snakesStartGUI[0] = 93;
+		} else {
+			snakesStartGUI[0] = 97;
+		}
+		if (getRandomBoolean() == true) {
+			snakesStartGUI[1] = 83;
+		} else {
+			snakesStartGUI[1] = 96;
+		}
+		if (getRandomBoolean() == true) {
+			snakesStartGUI[2] = 57;
+		} else {
+			snakesStartGUI[2] = 21;
+		}
+		if (getRandomBoolean() == true) {
+			snakesStartGUI[3] = 52;
+		} else {
+			snakesStartGUI[3] = 80;
+		}
+		if (getRandomBoolean() == true) {
+			snakesStartGUI[4] = 46;
+		} else {
+			snakesStartGUI[4] = 34;
+		}
+		if (getRandomBoolean() == true) {
+			snakesStartGUI[5] = 26;
+		} else {
+			snakesStartGUI[5] = 16;
+		}
+		setSnakesStart(snakesStartGUI);
+		System.out.println("snake start");
+	for (int i = 0; i < getSnakesStart().length; i++) {
+		System.out.println(getSnakesStart()[i]);
+	}
+		
+		
+		if (snakesStartGUI[0] == 93) {
+			snakesEndGUI[0] = 71;
+		} else {
+			snakesEndGUI[0] = 76;
+		}
+		if (snakesStartGUI[1] == 83) {
+			snakesEndGUI[1] = 65;
+		} else {
+			snakesEndGUI[1] = 74;
+		}
+		if (snakesStartGUI[2] == 57) {
+			snakesEndGUI[2] = 42;
+		} else {
+			snakesEndGUI[2] = 3;
+		}
+		if (snakesStartGUI[3] == 52) {
+			snakesEndGUI[3] = 47;
+		} else {
+			snakesEndGUI[3] = 58;
+		}
+		if (snakesStartGUI[4] == 46) {
+			snakesEndGUI[4] = 17;
+		} else {
+			snakesEndGUI[4] = 29;
+		}
+		if (snakesStartGUI[5] == 26) {
+			snakesEndGUI[5] = 9;
+		} else {
+			snakesEndGUI[5] = 7;
+		}
+		System.out.println("snakes end");
+		for (int i = 0; i < getSnakesStart().length; i++) {
+			System.out.println(snakesEndGUI[i]);
+		}
+	}
+	
+	public int[] getSnakesStart() {
+		return snakesStartGUI;
+	}
+	
+	public void setSnakesStart(int[] snakesStart) {
+		this.snakesStartGUI = snakesStart;
+	}
+	
+	public int[] getLaddersStart() {
+		return laddersStartGUI;
+	}
+	
+	public void setLaddersStart(int[] laddersStart) {
+		this.laddersStartGUI = laddersStart;
 	}
 	
 	public int dice() {
