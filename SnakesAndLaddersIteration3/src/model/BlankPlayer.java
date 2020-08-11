04c328a638d72a1b7eb2e5cdb472bf5caeb2b2ae
Arlina04 <67571039+Arlina04@users.noreply.github.com>
2020-08-10 20:53:21 -0600

    Update BlankPlayer.java

--- SnakesAndLaddersIteration3/src/model/BlankPlayer.java
+++ SnakesAndLaddersIteration3/src/model/BlankPlayer.java
@@ -2,6 +2,10 @@ package model;
 
 public class BlankPlayer extends Player {
 
+       /**
+	* This constructor creates spaces on the
+	* game board for each of the players.
+	*/
 	public BlankPlayer() {
 		this.name = "  ";
 		player = new String[101];
